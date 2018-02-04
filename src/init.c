586fcaeee52192e1fe263fe14fb3ded9e0dc2d6f
Alejandro Ramirez <armzprz@gmail.com>
2018-02-04 15:35:27 -0600

    Fix indentation and spacing

--- src/init.c
+++ src/init.c
@@ -11,47 +11,48 @@
 
 
 int main(int argc, char ** argv) {
-    //PID array for keeping track of children
-    pid_t* children;
-    children = malloc(INTENDED_CHILDREN * sizeof(pid_t));
+  //PID array for keeping track of children
+  pid_t * children;
+  children = malloc(INTENDED_CHILDREN * sizeof(pid_t));
 
-    //Control variables
-    int i;
-    int childen_tracker = 0;
-    pid_t pid; //current process PID
-    int status; //child process exit status
-    bool shutdown = false;
+  //Control variables
+  int i;
+  int childen_tracker = 0;
+  pid_t pid; //current process PID
+  int status; //child process exit status
+  bool shutdown = false;
 
-    //child proceses initialisation
-    for(i = 0; i < INTENDED_CHILDREN; i++){
-        pid = fork();
-        if(pid == 0){
-            execl("/usr/bin/xterm","-e","../bin/time",(char*)NULL);
-        } else{
-            children[childen_tracker++] = pid;
-        }
+  //child proceses initialisation
+  for(i = 0; i < INTENDED_CHILDREN; i++) {
+    pid = fork();
+    if(pid == 0) {
+      execl("/usr/bin/xterm","-e","./bin/getty", (char*) NULL);
+    } else{
+      children[childen_tracker++] = pid;
     }
+  }
 
-    //main loop
-    while(!shutdown){
-        //check children individually
-        for(childen_tracker = 0; childen_tracker < INTENDED_CHILDREN;childen_tracker
-            if(waitpid(children[childen_tracker], &status, WNOHANG) != 0){
-                //TODO check if double check is redundant
-                if(WIFEXITED(status)){
-                    //TODO properly check status to start shutdown procedure
-                    //TODO investigar SIGTERM y SIGKILL
+  //main loop
+  while(!shutdown) {
+    //check children individually
+    for(childen_tracker = 0; childen_tracker < INTENDED_CHILDREN; childen_tracker) {
+      if(waitpid(children[childen_tracker], &status, WNOHANG) != 0) {
+        //TODO check if double check is redundant
+        if(WIFEXITED(status)) {
+          //TODO properly check status to start shutdown procedure
+          //TODO investigar SIGTERM y SIGKILL
 
-                    //restart child process
-                    pid = fork();
-                    if(pid == 0){
-                        execl("/usr/bin/xterm","-e","./time",(char*)NULL);
-                    } else {
-                        printf("Proceso %d terminado iniciando nuevo proceso %d\n", children[childen_tracker],pid);
-                        children[childen_tracker] = pid;
-                    }
-                }
-            }
+          //restart child process
+          pid = fork();
+          if(pid == 0) {
+            execl("/usr/bin/xterm","-e","./bin/getty",(char*)NULL);
+          } else {
+            printf("Process %d finished. Spawining process %d\n",
+                children[childen_tracker], pid);
+            children[childen_tracker] = pid;
+          }
         }
+      }
     }
+  }
 }
