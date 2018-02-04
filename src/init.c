09a9bb54eefde965ca42405f50ff595a4559fda9
Daniel Orozco <danybeam@gmail.com>
2018-02-04 13:18:28 -0600

    Changed comments language to english

--- src/init.c
+++ src/init.c
@@ -4,50 +4,45 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-//numero de procesos hijos que seran creados
+#include "utils/types.h"
+
+// Children proceses to be created
 #define INTENDED_CHILDREN 6
 
-//bool no existe en C yay!
-typedef int bool;
-#define true 1
-#define false 0
 
-//punto de entrada, no necesito los argumentos pero ya estaban en el template y noafectan en nada
 int main(int argc, char ** argv) {
-    //arreglo con los PIDs de los procesos hijos
+    //PID array for keeping track of children
     pid_t* children;
     children = malloc(INTENDED_CHILDREN * sizeof(pid_t));
 
-    //variables de control
-    int i; //good ol' reliable i
-    int childen_tracker = 0; //pude haber usado i pero es mas facil saber que estoy buscando con esta variable
-    pid_t pid; //PID del proceso
-    int status; //status de como termino el proceso
-    bool shutdown = false; //checa si ya terminamos el experimento
+    //Control variables
+    int i;
+    int childen_tracker = 0;
+    pid_t pid; //current process PID
+    int status; //child process exit status
+    bool shutdown = false;
 
-    //creacion de procesos hijos inicial similar a "arranca.c" de la practica 1
+    //child proceses initialisation
     for(i = 0; i < INTENDED_CHILDREN; i++){
         pid = fork();
         if(pid == 0){
-            execl("/usr/bin/xterm","-e","./time",(char*)NULL);
+            execl("/usr/bin/xterm","-e","../bin/time",(char*)NULL);
         } else{
             children[childen_tracker++] = pid;
         }
     }
 
-    //mientras no haya terminado sigue por siempre
+    //main loop
     while(!shutdown){
-        //verifica  cada hijo eventualmente
-        for(childen_tracker = 0; childen_tracker < INTENDED_CHILDREN;childen_tracker++){
-            //Si ya termino el proceso lo reinicia
+        //check children individually
+        for(childen_tracker = 0; childen_tracker < INTENDED_CHILDREN;childen_tracker
             if(waitpid(children[childen_tracker], &status, WNOHANG) != 0){
-                //TODO Probar redundancia de doble chequeo
+                //TODO check if double check is redundant
                 if(WIFEXITED(status)){
-                    //TODO condicion para recibir estado de error y iniciar procedimiento de shutdown
+                    //TODO properly check status to start shutdown procedure
                     //TODO investigar SIGTERM y SIGKILL
 
-                    //TODO extraer a metodo independiente para evitar repetir codigo:
-                    //Si no hace falta hacer shutdown se crea proceso nuevo
+                    //restart child process
                     pid = fork();
                     if(pid == 0){
                         execl("/usr/bin/xterm","-e","./time",(char*)NULL);
