4d08d826969b3f02cc82e94f2df1eda766a36c7d
Alejandro Ramirez <armzprz@gmail.com>
2018-02-04 21:23:13 -0600

    Base sh execution
    Input parsing

--- src/sh.c
+++ src/sh.c
@@ -1,9 +1,70 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "utils/io.h"
+#include "utils/types.h"
+
+#define ARGC 5
+
+char * PATH;
+// Prompt string
+char * PS = "bush $ ";
+
+typedef struct {
+  char * exec;
+  char ** args;
+  int argc;
+} command;
+
+void set_path();
+command * parse_input(char*);
+bool is_command(command*, char*);
 
 int main(int argc, char ** argv) {
-  printf("Hello, sh!\n");
-  int a;
-  printf("insert anything: ");
-  scanf("%d", &a);
+  set_path();
+  while(true) {
+    char * input = prompt(PS);
+    command * c = parse_input(input);
+    int i;
+    for(i = 0; i < c->argc; i++) puts(c->args[i]);
+    execv(c->exec, c->args);
+  }
+
   return 0;
+}
+
+void set_path() {
+  PATH = getenv("PATH");
+}
+
+command * parse_input(char * input) {
+  char * token = strtok(input, " ");
+  if(token != NULL) {
+    command * c = (command *) malloc(sizeof(command *));
+    char * command = token;
+
+    char ** args = (char **) malloc(sizeof(char*) * ARGC);
+    int argcount = 0;
+
+    while(token != NULL) {
+      token = strtok(NULL, " ");
+      args[argcount++] = token;
+    }
+    if(argcount == 0) {
+      args[0] = "";
+    }
+    argcount--;
+
+    c->exec = command;
+    c->args = args;
+    c->argc = argcount;
+    return c;
+  } else {
+    return NULL;
+  }
+}
+
+bool is_command(command * c, char * str) {
+  return strcmp(c->exec, str) == 0 ? true : false;
 }
